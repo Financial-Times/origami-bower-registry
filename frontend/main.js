1f94d36ab2891b1dc739269660693cf06cc533f7
chee <chee@snoot.club>
2021-06-21 13:16:34 +0100

    Build css and js from source

---
+++ frontend/main.js
@@ -0,0 +1,11 @@
+import Layout from "@financial-times/o-layout";
+// import Fonts from "@financial-times/o-fonts";
+import HeaderServices from "@financial-times/o-header-services";
+// import FooterServices from "@financial-times/o-footer-services";
+import SyntaxHighlight from "@financial-times/o-syntax-highlight";
+
+Layout.init();
+// Fonts.init();
+HeaderServices.init();
+// FooterServices.init();
+SyntaxHighlight.init();
